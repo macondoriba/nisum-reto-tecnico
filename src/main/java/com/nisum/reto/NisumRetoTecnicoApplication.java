26a257f3d0945bdcf146a72cb62df7d0d02549ae
macondoriba <m.condori.b@outlook.com>
2024-06-08 16:10:16 -0500

    primer commit

---
+++ src/main/java/com/nisum/reto/NisumRetoTecnicoApplication.java
@@ -0,0 +1,13 @@
+package com.nisum.reto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NisumRetoTecnicoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NisumRetoTecnicoApplication.class, args);
+    }
+
+}
