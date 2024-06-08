b51dcb744abb151771d634f86992cae29a60a5de
macondoriba <m.condori.b@outlook.com>
2024-06-08 16:10:16 -0500

    primer commit

---
+++ src/test/java/com/nisum/reto/NisumRetoTecnicoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nisum.reto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NisumRetoTecnicoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
